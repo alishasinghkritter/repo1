1795fdfa082ec44a4dafcfa6f34ff19eda137ca7
alishasinghkritter <alisha@kritter.in>
2020-11-23 14:48:24 +0530

    water

---
+++ hungry.py
@@ -0,0 +1,9 @@
+hungry=input("are you hungry?")
+if hungry == "yes":
+	print("eat apple");
+	print("eat pizza");
+	print("eat burger");
+else:
+	thirsty =input("are you thristy?");
+	if thirsty=="yes";
+		print("drink water");
