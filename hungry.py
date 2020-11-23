483be8f6d327e6d147ccc82145c642baa042febb
alishasinghkritter <alisha@kritter.in>
2020-11-23 15:07:31 +0530

    Revert "water"
    This reverts commit 1795fdfa082ec44a4dafcfa6f34ff19eda137ca7.

--- hungry.py
+++
@@ -1,9 +0,0 @@
-hungry=input("are you hungry?")
-if hungry == "yes":
-	print("eat apple");
-	print("eat pizza");
-	print("eat burger");
-else:
-	thirsty =input("are you thristy?");
-	if thirsty=="yes";
-		print("drink water");
